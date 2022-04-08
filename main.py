724296fb7e13657b3f074ecb1ae56b55b6b8d848
remble1 <pRt8zfuPUwb9>
2022-04-09 01:38:02 +0200

    connect slider with label

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from PyQt5.QtWidgets import QSlider, QLabel
 from PyQt5 import QtWidgets, uic 
 import sys 
+
+
 
 class Ui(QtWidgets.QMainWindow):
   def __init__(self):
     super(Ui, self).__init__()
     uic.loadUi('D:\program_pola\MainWindowSample1.ui', self)
     self.show()
+    self.ui = Ui
+    self.dlugoscOdcinkaSlider = self.findChild(QSlider, "dlugoscOdcinkaSlider")
+    self.labelDlugoscOdcinka = self.findChild(QLabel, "labelDlugoscOdcinka")
+    self.dlugoscOdcinkaSlider.valueChanged.connect(self.number_changed)
+
+  def number_changed(self):
+    new_value = str(self.dlugoscOdcinkaSlider.value())
+    self.labelDlugoscOdcinka.setText(new_value)
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
